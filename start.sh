9d465059e1970c4220f84e0e7d8a23a71abb1c8c
Santa claus <santa@nortpole.com>
2021-06-13 20:14:28 +0300

    add manage.py contents

---
+++ start.sh
@@ -0,0 +1,6 @@
+export MAIL_USERNAME='johnstoneshylock@gmail.com'
+export MAIL_PASSWORD='AbigaelBrayo'
+export SECRET_KEY='yegon'
+export DATABASE_URL='postgresql+psycopg2://moringa:30196642@localhost/pitches-zone'
+
+python3.8 manage.py server
