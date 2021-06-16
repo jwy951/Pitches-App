e4f0f806264006b5f6a83ea5d58714a4c86c3bb3
Santa claus <santa@nortpole.com>
2021-06-16 11:41:53 +0300

    Updating start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 # export MOVIE_API_KEY='08563b904884eade562a50c9ebd04daa'
 export SECRET_KEY='30196642'
-export MAIL_USERNAME='johnstoneshylock@gmail.com'
-export MAIL_PASSWORD='AbigaelBray'
+export MAIL_USERNAME='yegon.johnstone@student.moringaschool.com'
+export MAIL_PASSWORD='@Jo30196642'
 
 python3.8 manage.py server
